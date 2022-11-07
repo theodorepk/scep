2e7973e0aee83361c2585f5354530e3ad008833d
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-11-07 23:07:37 +0100

    base scep

---
+++ components/Film.tsx
@@ -0,0 +1,35 @@
+const Film = () => {
+  return (
+    <div>
+      <div className="reuInfo">
+        <span>Saison</span>
+        <span> | </span>
+        <span>CM</span>
+        <span> | </span>
+        <span>Date</span>
+      </div>
+      <h2>Titre du film</h2>
+      <div className="filmInfo">
+        <div>
+          <span>Réalisateur</span>
+          <span> - </span>
+          <span> Année</span>
+          <span> - </span>
+          <span> Durée </span>
+        </div>
+      </div>
+      <div>
+        <span> - </span>
+        <span> image</span>
+        <span> + </span>
+      </div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error nihil
+        facilis nostrum fugit enim nam. Assumenda maxime rem labore sed
+        possimus. Earum asperiores vero quia reiciendis ex aperiam sed ratione.
+      </p>
+    </div>
+  );
+};
+
+export default Film;
