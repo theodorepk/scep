8d9eefca0ac8e90af0c15e15d411e3940827a63b
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-12-05 00:07:36 +0100

    modal

--- components/AddFilm/FilmSelected.tsx
+++ components/AddFilm/FilmSelected.tsx
@@ -40,28 +40,34 @@ const FilmSelected = ({ filmId, filmToAdd }: Props) => {
   }, [filmId]);
 
   return !isLoading ? (
-    <div className="border-solid border-2 border-blue-500">
-      <h2 className="underline">Film Selected</h2>
-      <div className="border-solid border border-slate-600">
-        <h3>Résalisateur</h3>
-        <span>
-          {credits.crew.filter((member) => member.job === "Director")[0].name}
-        </span>
-      </div>
-      <div className="border-solid border border-slate-600">
-        <h3>Titre</h3>
-        <span>{filmToAdd.title}</span>
-      </div>
-      <div className="border-solid border border-slate-600">
-        <h3>Résumé</h3>
-        <p>{filmToAdd.synopsis}</p>
-      </div>
-      <div className="border-solid border border-slate-600">
-        <h3>Année</h3>
-        <p>{filmToAdd.year}</p>
-      </div>
-      <div className="border-solid border border-slate-600">
-        <img src={filmToAdd.poster} />
+    <div className="fixed bg-white w-screen h-screen top-0 flex justify-center items-center bg-opacity-50">
+      <div className=" bg-black w-10/12 h-5/6 border-solid border-2 border-blue-500">
+        <div className="flex justify-between">
+          <h2 className="underline">Film Selected</h2>
+          <button>X</button>
+        </div>
+
+        <div className="border-solid border border-slate-600">
+          <h3>Résalisateur</h3>
+          <span>
+            {credits.crew.filter((member) => member.job === "Director")[0].name}
+          </span>
+        </div>
+        <div className="border-solid border border-slate-600">
+          <h3>Titre</h3>
+          <span>{filmToAdd.title}</span>
+        </div>
+        <div className="border-solid border border-slate-600">
+          <h3>Résumé</h3>
+          <p>{filmToAdd.synopsis}</p>
+        </div>
+        <div className="border-solid border border-slate-600">
+          <h3>Année</h3>
+          <p>{filmToAdd.year}</p>
+        </div>
+        <div className="border-solid border border-slate-600">
+          <img src={filmToAdd.poster} className="h-64" />
+        </div>
       </div>
     </div>
   ) : (
