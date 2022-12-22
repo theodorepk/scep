c6080287d4e45912c74b2f8c3fbc2150b9cc57b7
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-12-22 17:27:29 +0100

    route to change the current season

--- pages/api/global/index.ts
+++ pages/api/global/index.ts
@@ -11,7 +11,14 @@ export default async function addGlobal(
   if (req.method === "GET") {
     await connectMongo();
     const global: IGlobal = await Global.find();
-    console.log(global);
+    // console.log(global);
     res.status(200).json(global);
+  } else if (req.method === "POST") {
+    const { season } = req.body;
+    await connectMongo();
+    const global: IGlobal[] = await Global.find();
+    global[0].currentSeason = season;
+    await global[0].save();
+    res.status(200).json(`Current season has been changed to ${season}`);
   }
 }
